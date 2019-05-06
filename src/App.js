d12488f1619b2972926dc77c4ebb138c0ecfbe95
Josh Margulis <josh.margulis@gmail.com>
2019-05-06 10:59:54 -0400

    Getting basic layout for initial screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DatePicker from 'react-date-picker';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Option Grant Calculator</h1>
       </header>
+      <fieldset className="form-group">
+        <legend>Option Grant Info</legend>
+        <div className="form-group">
+          <label>Number of Shares Granted</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="form-group">
+          <label>Number of Shares Total</label>
+          <input className="form-control" type="text"/>
+        </div>
+        <div className="form-group">
+          <label>Strike Price</label>
+          <input className="form-control" type="text"/>
+        </div>
+        <div className="form-group">
+          <label>Strike Date</label>
+          <div className="d-block">
+            <DatePicker />
+          </div>
+        </div>
+      </fieldset>
+      <div>
+        <button className="btn btn-outline-primary">Add Another Grant</button>
+        <button className="btn btn-primary">Calculate Exit Value &gt;</button>
+      </div>
     </div>
   );
 }
